31c91f6cbdef9064e86584bc3cbb9324e5b91316
Candace Ahrends <candaceahrends@Candaces-Air.lan>
2021-01-18 17:47:55 -0500

    s

--- diary-microservice/routes/Activity.js
+++ diary-microservice/routes/Activity.js
@@ -11,7 +11,7 @@ router.post("/save", async function (req, res, next) {
     const { id, date, userId, qty, action, cal } = req.body.activity;
     console.log("databse getting id ", id, date, userId, action);
     let activityItem = {
-        id: `${id}-${date}`,
+        id: `${id}-${date}-${userId}`,
         userId: userId,
         date: date,
         hour: moment().format('HH'),
